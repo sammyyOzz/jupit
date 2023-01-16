b253d05c7b4d769e840cbd8ced5f7edabfd35b6e
Sammy <sammyoziegbe@gmail.com>
2023-01-16 16:56:03 +0100

    WIP: The top section of landing page

---
+++ src/components/card-one/index.tsx
@@ -0,0 +1,45 @@
+import { Box, Flex, Image, Text } from '@chakra-ui/react'
+
+interface CardOneProps {
+  image: string;
+  text: string;
+  top?: string;
+  right?: string;
+  left?: string;
+  bottom?: string;
+}
+
+function CardOne({ image, text, top, right, left, bottom }: CardOneProps) {
+  return (
+    <Flex 
+      borderRadius="30px" 
+      p="20px" 
+      justify="center" 
+      align="center" 
+      w="fit-content" 
+      boxShadow="2px 4px 8px rgba(0, 0, 0, 0.1)"
+      position="absolute"
+      top={top}
+      right={right}
+      left={left}
+      bottom={bottom}
+      bg="white"
+    >
+      <Image src={image} alt="" />
+      <Box pl={2}>
+        <Text 
+          mb="4px" 
+          color="#1C1C93" 
+          fontWeight={600} 
+          fontSize="14px"
+        >
+          { text }
+        </Text>
+        <Box bg="#D9D9D9" w="80%" h="5px" mb="5px" />
+        <Box bg="#D9D9D9" w="60%" h="5px" />
+      </Box>
+    </Flex>
+  )
+}
+
+export default CardOne
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,51 @@
-import React from 'react'
+import { Box, Flex, Heading, Text } from "@chakra-ui/react"
+import Button from "../components/button"
+import manOnLandingPage from '../assets/man-landing-page.png'
+import CardOne from "../components/card-one"
+import coinImage from "../assets/coin.png"
+import faceImage from "../assets/face.png"
 
 function Home() {
   return (
-    <div>Home</div>
+    <>
+      <Flex minH="calc(100vh - 100px)" py="50px">
+        <Flex direction={['column', null, null, 'row']} w="90%" m="auto" justify={[null, null, null, 'space-between']}>
+          <Box w={['100%', null, null, 'calc(50% - 10px)']} py="30px">
+            <Heading as="h1" fontSize="50px" color="black" lineHeight="70px">
+              Make easy digital assets transactions anywhere, anytime with Jupit.
+            </Heading>
+            <Text my="8" color="#040415" fontSize="20px" fontWeight={500}>
+              A place for everyone who wants to simply buy and sell digital assets, it just takes 120 seconds!
+            </Text>
+            <Button color="#1C1C93" bg="secondary" p="30px 40px">
+              Get Started
+            </Button>
+          </Box>
+          <Box
+            w={['100%', null, null, 'calc(50% - 10px)']}
+            bgImage={`url(${manOnLandingPage})`}
+            bgPosition="right"
+            bgRepeat="no-repeat"
+            position="relative"
+          >
+            <CardOne 
+              image={coinImage} 
+              text="Transaction Successful!"
+              right="0" 
+              top="220px" //!change this
+            />
+            <CardOne 
+              image={faceImage} 
+              text="Paul just sent you 500 usdt"
+              bottom="10px"
+              left="calc(50% - 100px)" //!change this
+            />
+          </Box>
+        </Flex>
+      </Flex>
+
+
+    </>
   )
 }
 
