2e0c8249b67d65235207fcc0b8d403fb2519f1a1
Sammy <sammyoziegbe@gmail.com>
2023-01-16 06:31:43 +0100

    setup chakra ui, header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,27 @@
+import { Box, Flex, Image, Text } from '@chakra-ui/react'
+import logo from '../../assets/logo.png'
+import { appRoutes } from '../../routes'
+import Button from '../button'
+
+function Layout() {
+  return (
+    <Box>
+      <Flex h="100px" w="90%" justify="space-between" align="center" m="auto">
+        <Image src={logo} alt="" h="50px" w="160px" />
+        <Flex justify="space-between">
+          {appRoutes.map(({ name, to }, index) => (
+            <Text key={index} color="#3D3D3D" fontSize="16px" fontFamily="Nexa" px="30px">
+              { name }
+            </Text>
+          ))}
+        </Flex>
+        <Box>
+          <Button outlined color="#1C1C93">Sign In</Button>
+          <Button outlined bg="secondary" color="#1C1C93">Register</Button>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
+
+export default Layout
---
+++ src/routes/index.tsx
@@ -0,0 +1,35 @@
+import { Route } from "../types"
+
+export const appRoutes: Array<Route> = [
+  {
+    name: 'About Us',
+    to: '#'
+  },
+  {
+    name: 'Product',
+    to: '#'
+  },
+  {
+    name: 'Resources',
+    to: '#'
+  },
+  {
+    name: 'Contact',
+    to: '#'
+  },
+]
+
+export const authRoutes: Array<Route> = [
+  {
+    name: 'Sign In',
+    to: '#'
+  },
+  {
+    name: 'Register',
+    to: '#'
+  },
+  {
+    name: 'Dashboard',
+    to: '#'
+  }
+]
