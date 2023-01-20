4c6a4cbb873d1717a6c9e48c9747fc409f4790b7
Sammy <sammyoziegbe@gmail.com>
2023-01-20 19:42:26 +0100

    convert app to pwa

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import theme from './theme';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -20,6 +21,11 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
